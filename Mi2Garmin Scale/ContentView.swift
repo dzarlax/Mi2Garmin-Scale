41c0e5f3e8c0192fcb8c4ef43fea0d9552695b4e
Alexey Panfilov <alexey@panfilov.net>
2024-02-16 11:26:50 +0100

    Initial Commit

---
+++ Mi2Garmin Scale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mi2Garmin Scale
+//
+//  Created by Alexey Panfilov on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mi2Garmin Scale/Mi2Garmin_ScaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mi2Garmin_ScaleApp.swift
+//  Mi2Garmin Scale
+//
+//  Created by Alexey Panfilov on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Mi2Garmin_ScaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
